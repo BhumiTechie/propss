1950459150fa102aeeaaedc205aca5a20dcfbb74
Bhumika gupta <bg0674079@gmail.com>
2024-04-19 18:51:56 +0530

    HigherOrder

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import Input from './Components/Input';
+
+const App = () => {
+  return (
+    <div>
+      <Input type="text" placeholder="Full Name" />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Components/Input.jsx
@@ -0,0 +1,27 @@
+
+import React, { useRef } from 'react';
+
+const Input = (props) => {
+  const showErr = useRef(null);
+
+  const changeHandler = () => {
+    const inp = showErr.current;
+    if (inp.value.length < 4) {
+		console.log(inp.value);
+      inp.style.border = "1px solid red";
+    } else {
+      inp.style.border = "1px solid black";
+    }
+  };
+
+  return (
+    <input
+      ref={showErr}
+      onChange={changeHandler}
+      className="border border-zinc-300 outline-none"
+      {...props}
+    />
+  );
+};
+
+export default Input;
